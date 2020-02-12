8062b611b3ad76e658565b0add595a26f024e7ff
Shu Saura <coyoteelabs@gmail.com>
2020-02-12 20:22:44 +0200

    Add the options form to the project

--- launcher-src/launcher.dpr
+++ launcher-src/launcher.dpr
@@ -4,7 +4,8 @@ uses
   Vcl.Forms,
   uMain in 'uMain.pas' {NotLauncherWindow},
   JsonDataObjects in 'JsonDataObjects\Source\JsonDataObjects.pas',
-  launcher_utils in 'launcher_utils.pas';
+  launcher_utils in 'launcher_utils.pas',
+  uOptions in 'uOptions.pas' {NotLauncherOptions};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TNotLauncherWindow, NotLauncherWindow);
+  Application.CreateForm(TNotLauncherOptions, NotLauncherOptions);
   Application.Run;
 end.
