604cb2ca6d2b56c7bfdd345a30c1153082b30fd3
Shu Saura <coyoteelabs@gmail.com>
2020-03-26 22:48:36 +0200

    Improved bootstrapper parameter detection

--- bootstrapper-src/bootstrapper_v2.dpr
+++ bootstrapper-src/bootstrapper_v2.dpr
@@ -9,10 +9,15 @@ uses
 
 var
    gamedir:string;
+   i:integer;
 
 begin
 gamedir := '';
-if (ParamStr(1)= '--gameDir') then gamedir := ParamStr(2);
+if gamedir = '' then
+   begin
+   for i:=1 to ParamCount-1 do
+       if (ParamStr(i)= '--gameDir') then gamedir := ParamStr(i+1);
+   end;
 
 if gamedir <> '' then ShellExecute(0,'open',PChar('launcher.exe'),PChar('--gameDir "'+gamedir+'"'), PChar(ExtractFilePath(ParamStr(0))), SW_SHOWNORMAL)
                  else ShellExecute(0,'open',PChar('launcher.exe'),nil, PChar(ExtractFilePath(ParamStr(0))), SW_SHOWNORMAL);
